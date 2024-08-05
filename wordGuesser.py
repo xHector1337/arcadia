9fe3f266319bfa5b3e1ba877b724942ee2499216
Umut <140441119+xHector1337@users.noreply.github.com>
2024-08-05 15:23:00 +0300

    random choice and word checker functions are better.

--- wordGuesser.py
+++ wordGuesser.py
@@ -1,14 +1,22 @@
 # Arcadia: The Land Of Fun!
 import random
 
-print("Welcome to the WordGuesser game!\nChoose a difficulty and start! You'll have attempts depend on the difficulty you choose.\nThe Word list contains adjectives, nouns and infinitive verbs without 'to'(There is not any special names!)\nInstead of guessing the whole word, try the find letters in the word by sending letters!\nGood luck!")
-words = ["cat","dog","hack","code","eye","face","house","white","blue","black","hear","red","pink","purple","consciousness","llama","eagle","falcon","man","woman","brother","sister","house","street","state","preload","load","code","congratulation","abandon","acquire","addict","rock","roll","punk","hard","agreement","aircraft","ship","learn","holiday","mister","sir","school","walk","meet","again","heart","know","where","sun","sunny","day","memory","moon","lake","live","die","death","cemetery","keyboard","mouse","computer","emotion","brain","run","word","justice","city","hall","angel","decision","result","laptop","absorb","destroy","people","fall","way","cowboy","cow","sea","see","light","flash","make","smile","snake","eater","solid","youth","school","homelander","deep","butcher","market","super"]
+print("Welcome to the WordGuesser game!\nChoose a difficulty and start! You'll have attempts depend on the difficulty you choose.\nThe Word list contains adjectives, nouns and infinitive verbs without 'to'(There is not any special names!)\nGood luck!")
+words = ["cat","dog","hack","code","eye","redemption","bandana","freedom","game","face","house","white","blue","black","hear","red","pink","purple","consciousness","llama","eagle","falcon","man","woman","brother","sister","house","street","state","preload","load","code","congratulation","abandon","acquire","addict","rock","roll","punk","hard","agreement","aircraft","ship","learn","holiday","mister","sir","school","walk","meet","again","heart","know","where","sun","sunny","day","memory","moon","lake","live","die","death","cemetery","keyboard","mouse","computer","emotion","brain","run","word","justice","city","hall","angel","decision","result","laptop","absorb","destroy","people","fall","way","cowboy","cow","sea","see","light","flash","make","old","smile","snake","eater","solid","youth","school","homelander","deep","butcher","market","super","good","bad","ugly","star","glaze","glance","perfect","winter","jungle","evil","fire","succession","title","liquid","theme","come","handkerchief","phone","console","mask","guess","empire","stay","interstellar","kitty","puppy"]
 # I heard those while listening to music :')
 
-def randomChoice(length):
-    word  = random.choice(words)
-    while len(word) != length:
-        word = random.choice(words)
+def randomChoice(difficulty):
+    word = random.choice(words)
+    match difficulty:
+        case "easy":
+            while len(word) != 3 and len(word) != 4:
+                word = random.choice(words)
+        case "medium":
+            while len(word) != 4 and len(word) != 5 and len(word) != 6:
+                word = random.choice(words)
+        case "hard":
+            while len(word) != 7 and len(word) != 8 and len(word) != 9 and len(word) != 10 and len(word) != 11 and len(word) != 12 and len(word) != 13 and len(word) != 14:
+                word = random.choice(words)                 
     return word
 
 def wordChecker(word,attempts):
@@ -16,18 +24,22 @@ def wordChecker(word,attempts):
         print(f"Oh, you are out of luck. The word was '{word}'")
         exit()
     user = str(input("Enter your guess!\n"))
-    while user != str(word):
-        found = str(word).find(user)
-        if found != -1:
-            print(f"You're close, {found+1}. character is '{word[found]}'")
-            attempts -= 1
-            print(f"You have {attempts} remaining attempts!")
-        elif found == -1:
-            attempts -=1 
+    if user == word:
+            print("You have found it!")
+            exit()
+    if len(user) > 0:
+        attempts -= 1
+        count = 0
+        for i in user:
+            if i in word:
+                count += 1
+                found = str(word).find(i)
+                print(f"You're close, {found+1}. character is '{word[found]}'")
+        if count == 0:
             print(f"Wrong guess!\nThe word is {'*'*len(word)}\nYou have {attempts} remaining attempts!")
-        wordChecker(word,attempts)
-    print(f"You have found it!")                             
-                    
+        else:
+            print(f"You have {attempts} remaining attempts!")    
+        wordChecker(word,attempts)               
         
 
 def menu():
@@ -36,40 +48,14 @@ def menu():
     choice = str(input("Choose your difficulty level:\n\t1 - Easy\n\t2 - Medium\n\t3 - Hard\n"))
     match choice:
         case "1":
-            number = random.randint(0,100)
             attempts = 5
-            if number > 50:
-                word = randomChoice(4)
-            else:
-                word = randomChoice(3)
+            word = randomChoice("easy")
         case "2":
             attempts = 4
-            number = random.randint(0,100)
-            if number > 15 and number < 50:
-                word = randomChoice(4)
-            elif number < 15:
-                word = randomChoice(5)
-            else:
-                word = randomChoice(6)
+            word = randomChoice("medium")
         case "3":
             attempts = 4
-            number = random.randint(0,100)
-            if number < 20 and number > 5:
-                word = randomChoice(14)
-            elif number >= 20 and number < 30:
-                word = randomChoice(7)
-            elif number >= 30 and number < 45:
-                word = randomChoice(8)
-            elif number >= 45 and number < 59:
-                word = randomChoice(9)
-            elif number >= 59 and number < 70:
-                word = randomChoice(10)
-            elif number >= 70 and number < 79:
-                word = randomChoice(11)
-            elif number >= 79 and number < 90:
-                word = randomChoice(12)
-            elif number >= 90 and number < 100:
-                word = randomChoice(13)
+            word = randomChoice("hard")
         case _:
             print("Please enter a right number!")
             menu()
