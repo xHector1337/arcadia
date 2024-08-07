5a62502beb64cb752696e999c3249493f774721a
Umut <140441119+xHector1337@users.noreply.github.com>
2024-08-07 11:18:32 +0300

    Added more commands and a basic parser

--- terminal.py
+++ terminal.py
@@ -1,5 +1,6 @@
 import re
 import os
+import requests
 
 users = ["root","hector"]
 def login():
@@ -20,30 +21,88 @@ def whoami(user):
     echo(user)    
 def su():
     terminal()
+def wget(url):
+    req = requests.get(url)
+    print(req.text)    
+def foxsay():
+    a = r"""
+   /\   /\   Todd Vargo
+  //\\_//\\     ____
+  \_     _/    /   /
+   / * * \    /^^^]
+   \_\O/_/    [   ]
+    /   \_    [   /
+    \     \_  /  /
+     [ [ /  \/ _/
+    _[ [ \  /_/
+    """
+    print(a)    
 def echo(arg):
     print(arg)
 def clear():
     if os.name == "nt":
         os.system("cls")
     else:
-        os.system("clear")        
+        os.system("clear")   
         
+def parser(arg,username):
+    if '&&' in arg and "'&&'" not in arg:
+        line = arg.split(' && ')
+        for i in line:
+            if "clear" in i and "'clear'" not in i:
+                clear()             
+            elif "echo" in i and "'echo'" not in i:
+                match = re.search(r"'([^']+)'",i)
+                if match:
+                    echo(match.group(1))
+            elif "echo" in i and "'" not in i:
+                echo("Usage: echo 'arg'")
+            elif "su" in i and "'su'" not in i:
+                su()
+            elif "whoami" in i and "'whoami'" not in i:
+                whoami(username)
+            elif "exit" in i and "'exit'" not in i:
+                echo("Exiting...")
+            elif "foxsay" in i and "'foxsay'" not in i:
+                foxsay()
+            elif "wget" in i and "'wget'" not in i:
+                match = re.search(r"'([^']+)'",i)
+                if match:
+                    wget(match.group(1))
+            elif "wget" in i and "'" not in i:
+                echo("Usage: wget 'url'")                    
+            else:
+                echo("An error acquired, undefined command found!")
+                break
+    elif "&&" not in arg:
+        if "clear" in arg and "'clear'" not in arg:
+            clear()
+        elif "echo" in arg and "'echo'" not in arg:
+            match = re.search(r"'([^']+)'",arg)
+            if match:
+                echo(match.group(1))
+        elif "echo" in arg and "'" not in arg:
+            echo("Usage: echo 'arg'")
+        elif "su" in arg and "'su'" not in arg:
+            su()
+        elif "whoami" in arg and "'whoami'" not in arg:
+            whoami(username)
+        elif "exit" in arg and "'exit'" not in arg:
+            echo("Exiting...")
+        elif "foxsay" in arg and "'foxsay'" not in arg:
+            foxsay()
+        elif "wget" in arg and "'wget'" not in arg:
+            match = re.search(r"'([^']+)'",arg)
+            if match:
+                wget(match.group(1))            
+        elif "wget" in arg and "'" not in arg:
+            echo("Usage: wget 'url'")
+        else:
+            echo("An error acquired, undefined command found!")                                                          
 def terminal():
     username = login()
     user = ""
     while user != "exit":
         user = str(input(f"{username}@arcadia> "))
-        if user.startswith("echo") and "'" in user:
-            match = re.search(r"'([^']+)'", user)
-            if match:
-                echo(match.group(1))
-        elif user.startswith("echo") and "'" not in user:
-            echo("Usage: echo 'arg'")
-        if "clear" in user:
-            clear()
-        if "whoami" in user:
-            whoami(username)
-        if 'su' in user:
-            su()
-                                      
-terminal()
+        parser(user,username)                 
+terminal()   
