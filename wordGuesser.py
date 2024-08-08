33ccbc9424fd30699a3e1000e55ade53a3eea913
Umut <140441119+xHector1337@users.noreply.github.com>
2024-08-08 15:01:20 +0300

    better hint system

--- wordGuesser.py
+++ wordGuesser.py
@@ -1,8 +1,7 @@
 # Arcadia: The Land Of Fun!
 import random
 
-print("Welcome to the WordGuesser game!\nChoose a difficulty and start! You'll have attempts depend on the difficulty you choose.\nThe Word list contains adjectives, nouns and infinitive verbs without 'to'(There is not any special names!)\nGood luck!")
-words = ["cat","dog","hack","code","eye","redemption","bandana","wise","freedom","game","face","house","white","blue","black","hear","red","pink","purple","wind","consciousness","llama","eagle","falcon","man","woman","brother","sister","life","paint","angry","river","house","street","state","preload","load","code","congratulation","abandon","acquire","addict","rock","roll","punk","hard","agreement","aircraft","ship","learn","holiday","mister","sir","school","walk","meet","again","heart","teacher","mom","dad","father","","know","where","sun","sunny","day","memory","moon","lake","live","die","death","cemetery","keyboard","mouse","computer","emotion","brain","run","word","justice","city","hall","angel","despicable","decision","result","laptop","absorb","destroy","people","fall","way","cowboy","cow","drive","driver","sea","see","light","flash","make","old","smile","snake","eater","solid","youth","school","homelander","deep","butcher","monkey","sloth","market","super","good","bad","ugly","star","glaze","glance","perfect","winter","jungle","boy","evil","fire","succession","title","liquid","theme","come","handkerchief","happy","girl","phone","console","mask","guess","empire","stay","ocean","climb","jump","love","interstellar","kitty","sell","world","puppy","west","north","east","south","soap","have","take","personal","alone","care","tired"]
+words = ["cat","dog","hack","code","eye","redemption","clear","bandana","wise","freedom","game","face","house","white","blue","black","hear","red","pink","purple","wind","consciousness","llama","eagle","falcon","man","woman","brother","sister","life","paint","angry","river","house","street","state","preload","load","code","congratulation","abandon","acquire","addict","rock","roll","punk","hard","agreement","aircraft","ship","learn","holiday","mister","sir","school","walk","meet","again","heart","teacher","mom","dad","father","","know","where","sun","sunny","day","memory","moon","lake","live","die","death","cemetery","keyboard","mouse","computer","emotion","brain","run","word","justice","city","hall","angel","despicable","decision","result","laptop","absorb","destroy","people","fall","way","cowboy","cow","drive","driver","sea","see","light","flash","make","old","smile","snake","eater","solid","youth","school","homelander","deep","butcher","monkey","sloth","market","super","good","bad","ugly","star","glaze","glance","perfect","winter","jungle","boy","evil","fire","succession","title","liquid","theme","come","handkerchief","happy","girl","phone","console","mask","guess","empire","stay","ocean","climb","jump","love","interstellar","kitty","sell","world","puppy","west","north","east","south","soap","have","take","personal","alone","care","tired"]
 # I heard those while listening to music :')
 
 def randomChoice(difficulty):
@@ -20,6 +19,7 @@ def randomChoice(difficulty):
     return word
 
 def wordChecker(word,attempts,hints):
+    stored = [""]
     if int(attempts) == 0:
         print(f"Oh, you are out of luck. The word was '{word}'")
         exit()
@@ -30,6 +30,9 @@ def wordChecker(word,attempts,hints):
     if user == "hint" and hints > 0:
         hints -=1
         h = random.randint(0,(len(word)-1))
+        while h in stored:
+            h = random.randint(0,(len(word)-1))
+        stored.append(h)
         print(f"There you go, here is your hint:\n{h+1}. character of the word is {word[h]}")
         wordChecker(word,attempts,hints)
     elif hints == 0 and user == "hint":
@@ -54,6 +57,7 @@ def wordChecker(word,attempts,hints):
         
 
 def menu():
+    print("Welcome to the WordGuesser game!\nChoose a difficulty and start! You'll have attempts depend on the difficulty you choose.\nThe Word list contains adjectives, nouns and infinitive verbs without 'to'(There is not any special names!)\nGood luck!")
     attempts = 0
     hints = 3
     word = ""
@@ -77,4 +81,3 @@ def menu():
     print(f"The word is {'*'*len(word)}")        
     print(f"The word's length is {len(word)}. You have {attempts} attempts and {hints} hints!\nYou can use your hints by typing 'hint'\nGood luck.")
     wordChecker(word,attempts,hints)
-menu()
